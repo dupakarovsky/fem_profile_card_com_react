ea624ba3bd60e0df9f4883e564c993b1b74c4487
Dupakarovsky <dupakarovsky@outlook.com>
2023-07-01 19:36:38 -0300

    initial commit

---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+// const OpenProps = require("open-props");
+// const postcssJitProps = require("postcss-jit-props");
+// const cssnano = require("cssnano");
+const autoprefixer = require("autoprefixer");
+const postcssPresetEnv = require("postcss-preset-env");
+const postcssImport = require("postcss-import");
+
+module.exports = {
+   plugins: [
+      postcssImport,
+      autoprefixer,
+      postcssPresetEnv({
+         stage: 0,
+      }),
+      //   postcssJitProps(OpenProps),
+      // cssnano,
+   ],
+};
